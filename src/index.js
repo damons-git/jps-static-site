cdfcaa530f567df7c385ce7513f3acce8689d971
Damon Sweeney <me@damons.email>
2020-07-19 16:39:07 +0100

    Add JS option highlighting to nav menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+// Import SCSS
 import './sass/_custom.scss'
 import './sass/nav.scss'
 import './sass/stylesheet.scss'
+
+// Import local JS
+import './js/nav.js'
+
+// Import JS libs
 import 'node-emoji'
 import '@fortawesome/fontawesome-free/js/all.min.js'
---
+++ src/js/nav.js
@@ -0,0 +1,37 @@
+// JS script to decrease opacity of all navigation menu options
+// that are not in mouse focus.
+//
+// This script is only active for screen sizes where the header based navigation is present.
+// This is device screens greater than 800px in width.
+
+function navOpacity() {
+    if (window.innerWidth > 800) {
+        let navItems = document.querySelectorAll(".nav-link")
+
+        navItems.forEach(element => {
+            let siblingArr = Array.from(navItems)
+            let currentIndex = siblingArr.indexOf(element)
+            siblingArr.splice(currentIndex, 1)
+
+            // Decrease opacity of non-hovered nav items on mouse over
+            element.addEventListener("mouseover", (event) => {
+                siblingArr.forEach(elem => {
+                    elem.querySelector("span").style.opacity = 0.35;
+                    elem.querySelector("i").style.opacity = 0.35;
+                })
+            })
+
+            // Increase opacity of non-hovered nav items on mouse out
+            element.addEventListener("mouseout", (event) => {
+                siblingArr.forEach(elem => {
+                    elem.querySelector("span").style.opacity = 1.0;
+                    elem.querySelector("i").style.opacity = 1.0;
+                })
+            })
+        })
+    }
+}
+
+window.addEventListener('load', () => {
+    navOpacity()
+})
