f24b6139faf4b5502b1eeae4b2f75d59d8671bbc
Damon Sweeney <me@damons.email>
2020-07-19 16:40:29 +0100

    Remove unnecessary screen size condition for nav highlighting script

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,31 +5,29 @@
 // This is device screens greater than 800px in width.
 
 function navOpacity() {
-    if (window.innerWidth > 800) {
-        let navItems = document.querySelectorAll(".nav-link")
+    let navItems = document.querySelectorAll(".nav-link")
 
-        navItems.forEach(element => {
-            let siblingArr = Array.from(navItems)
-            let currentIndex = siblingArr.indexOf(element)
-            siblingArr.splice(currentIndex, 1)
+    navItems.forEach(element => {
+        let siblingArr = Array.from(navItems)
+        let currentIndex = siblingArr.indexOf(element)
+        siblingArr.splice(currentIndex, 1)
 
-            // Decrease opacity of non-hovered nav items on mouse over
-            element.addEventListener("mouseover", (event) => {
-                siblingArr.forEach(elem => {
-                    elem.querySelector("span").style.opacity = 0.35;
-                    elem.querySelector("i").style.opacity = 0.35;
-                })
-            })
-
-            // Increase opacity of non-hovered nav items on mouse out
-            element.addEventListener("mouseout", (event) => {
-                siblingArr.forEach(elem => {
-                    elem.querySelector("span").style.opacity = 1.0;
-                    elem.querySelector("i").style.opacity = 1.0;
-                })
+        // Decrease opacity of non-hovered nav items on mouse over
+        element.addEventListener("mouseover", (event) => {
+            siblingArr.forEach(elem => {
+                elem.querySelector("span").style.opacity = 0.35;
+                elem.querySelector("i").style.opacity = 0.35;
             })
         })
-    }
+
+        // Increase opacity of non-hovered nav items on mouse out
+        element.addEventListener("mouseout", (event) => {
+            siblingArr.forEach(elem => {
+                elem.querySelector("span").style.opacity = 1.0;
+                elem.querySelector("i").style.opacity = 1.0;
+            })
+        })
+    })
 }
 
 window.addEventListener('load', () => {
